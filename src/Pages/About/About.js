0590143bacf3abcaf875176cbb2e3e2286d9ed86
Rezaul Karim <spaceled@gmail.com>
2022-08-04 22:23:51 +0600

    First Commit

---
+++ src/Pages/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// https://web.programming-hero.com/update-1/video/update-1-71-3-create-simple-top-banner-using-hero-component
+
+const About = () => {
+  return (
+    <div>
+      <h2>This is about</h2>
+    </div>
+  );
+};
+
+export default About;
